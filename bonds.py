cb2de63d53862196d51c585689e722426a2e54ac
RomanMishel <sealaesone@gmail.com>
2024-05-28 22:38:16 +0300

    Update bonds.py

--- bonds.py
+++ bonds.py
@@ -1,23 +1,27 @@
 import requests
 import json
 
+currency = input(f'You currency:'"")
+# print(currency)
 def crypto_currency_request():
     url = "https://api.livecoinwatch.com/coins/single"
-    
-    payload = json.dumps({
-    "currency": "TON",
-    "code": "EQCajaUU1XXSAjTD-xOV7pE49fGtg4q8kF3ELCOJtGvQFQ2C",
+    payload = {
+    "currency": "USD",
+    "code": currency,
     "meta": False
-    })
+    }
     headers = {
     'content-type': 'application/json',
     'x-api-key': 'c2ea5b77-1668-46f1-9089-e1691c6ff332'
     }
 
     try:
-        response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
+        response = requests.request("POST", url, headers=headers, json=payload, timeout=10)
+        response_json= response.json()
+        price = response_json['rate']
+        print(price)
         response.raise_for_status()
-        return response.json()
+        return response.json(), price 
     
     except requests.exceptions.HTTPError as http_err:
         print(f'HTTP error: {http_err}')
@@ -32,32 +36,27 @@ def crypto_currency_request():
         print(f'Error: {req_err}')
         return None
 
-def crypto_currency_price(data):
+def crypto_currency_price(data, price):
     if data is None:
         print("Data is None. Request was not successful.")
         return
-
-    
-    for coin in data.get("data", []):
-        if coin.get("symbol") == "TON":
-            price = coin.get("price")
-            if price is not None:
-                low_price = 1.17 
-                max_price = 6
-                print(f'Current price: {price}')
-                if price <= low_price:
-                    print(f'The price is {price} low, time to buy!')
-                elif price >= max_price:
-                    print(f'The price is {price} high, time to sell!')
-                else:
-                    print(f'The price is {price}, no action needed.')
-            else:
-                print("Price not found for TONCOIN.")
-            return 
-    print("TONCOIN not found in the response data.")
-
+ 
+    if price is not None:
+        low_price = 1.17 
+        max_price = 6
+        print(f'Current price: {price}')
+        if price <= low_price:
+            print(f'The price is {price} low, time to buy!')
+        elif price >= max_price:
+            print(f'The price is {price} high, time to sell!')
+        else:
+            print(f'The price is {price}, no action needed.')
+    else:
+        print("Price not found for .")
+        
 def crypto_monitoring():
-    data = crypto_currency_request()
-    crypto_currency_price(data)
+    data, price = crypto_currency_request()
+    print(data)
+    crypto_currency_price(data, price)
 
 crypto_monitoring()
