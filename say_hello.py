e1be4084fa6dd6db951910c2107046fdd91a134f
RomanMishel <sealaesone@gmail.com>
2026-06-13 12:21:28 +0300

    Update say_hello.py

--- say_hello.py
+++ say_hello.py
@@ -2,5 +2,5 @@ def say_hello():
     print("Hello, write you name, please:")
     name = input()
     print(f"Hello,{name}, Nice to meet you!")
-    print("Programm terminated")
+    print("Programm terminated.")
 say_hello()
