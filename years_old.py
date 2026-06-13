214e759dca8e7a922d7e41517fc62c2fbf30d14c
RomanMishel <sealaesone@gmail.com>
2026-06-13 12:25:47 +0300

    Create years_old.py

---
+++ years_old.py
@@ -0,0 +1,7 @@
+def years_old():
+    print("Hello, write your year of birth:")
+    x = int(input())
+    y = 2026 - x
+    print(f"You are {y} years old.")
+    print("Programm terminated.")
+years_old()
