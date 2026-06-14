fef6442ffb55231baa166d3642c15467b408b2f6
RomanMishel <sealaesone@gmail.com>
2026-06-14 20:03:54 +0300

    Create death_counter.py

---
+++ death_counter.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+def death_counter():
+    window = tk.Tk()
+
+    window.title("Death Counter")
+
+    window.geometry("400x200")
+
+    label = tk.Label(window, text="Death Counter", font=("Arial", 20))
+
+    death_counter = int(0)
+
+    user_input = "."
+
+    def add_death():
+        if user_input.get() == ".":
+            death_counter += 1
+            label.config(text=f"Deaths count: {death_counter}")
+
+    def reset_death():
+        if user_input.get() == "Reset":
+            death_counter = 0
+
+    button = tk.Button(window, text="+", font=("Arial", 20), command=add_death)
+
+    button.pack(pady=20)
+
+    button = tk.Button(window, text="Reset", font=("Arial", 20), command=reset_death)
+
+    button.pack(pady=20)
+
+    window.mainloop()
+
+death_counter()
