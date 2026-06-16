56f70a000070a3a5ff438742f9bfcad895ca3d69
RomanMishel <sealaesone@gmail.com>
2026-06-16 20:24:54 +0300

    Create from_one_to_hundred.py

---
+++ from_one_to_hundred.py
@@ -0,0 +1,12 @@
+
+
+def from_one_to_hundred():
+    counter = 0
+    print(counter)
+    while():
+        if counter != 100:
+            counter += 1
+            print(counter)
+        else:
+            return
+from_one_to_hundred()
