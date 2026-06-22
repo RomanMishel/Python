64259476621f0202891c9eac2efa0966e096548a
RomanMishel <sealaesone@gmail.com>
2026-06-22 12:23:14 +0300

    Create guess_a_number.py

---
+++ guess_a_number.py
@@ -0,0 +1,31 @@
+import random
+
+guessed_num = ""
+
+num_list = [
+    1,2,3,4,5,6,7,8,9
+]
+
+user_input = int(input("Guess a number: "))
+
+def guess(user_input):
+    random_num = random.choice(num_list)
+    
+
+    if random_num < user_input:
+        print("You guessed wrong, you number is too high")
+        print(random_num)
+        return 
+    elif random_num > user_input:
+        print("You guessed wrong, you number is too low")
+        print(random_num)
+        return 
+    elif random_num == user_input:
+        print("You guessed right!")
+        print(random_num)
+        return 
+    else:
+        print("error")
+        return
+
+guess(user_input)
