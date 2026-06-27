f3861cd8487ef6a9d68aceb579929156e0c6a13b
RomanMishel <sealaesone@gmail.com>
2026-06-27 21:26:57 +0300

    Update contacts_book.py

--- contacts_book.py
+++ contacts_book.py
@@ -7,10 +7,12 @@ contacts_list = {
 user_input = input(str("Please choose action: \n1.Add Contact \n2.Show All Contacts \n.3Remove Contact \n4.Edit Contact \n5.Exit"))
 
 def add_contact():
-    contact_name = input(str("Enter a contact name: "))
-    contact_email = input(str("Enter a contact email: "))
-    contact_phone = input(str("Enter a contact phone: "))
+    contact_name = input("Enter a contact name: ")
+    contact_email = input("Enter a contact email: ")
+    contact_phone = input("Enter a contact phone: ")
 
-    contacts_list.append(contact_name, contact_email, contact_phone)
+    contact_name["name"] = contact_name
+    contact_email["email"] = contact_email
+    contact_phone["phone"] = contact_phone
     print("Contact Added!")
 
