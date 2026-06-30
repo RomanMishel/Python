93a955753ab3084f2ffb9f7e00471a976b3ac1c3
RomanMishel <sealaesone@gmail.com>
2026-06-30 15:34:11 +0300

    upd

---
+++ currency_converter.py
@@ -0,0 +1,14 @@
+currencies = {
+    "USD" : "1.00",
+    "JPY" : "149.50",
+    "EUR" : "0.92",
+}
+def converter(amount):
+    final_amount = amount * currencies
+    print(f"You have {currencies['USD']} in US Dollars\n {currencies['JPY']} in Japanies Yens\n {currencies['EUR']} in Euros")
+
+
+
+
+
+
