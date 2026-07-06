9a97d40a8afe9ef4a14637778c6dbd32b8f77b4f
RomanMishel <sealaesone@gmail.com>
2026-07-06 20:41:20 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 from random import choice, random
 
+def hangman():
+    wordlist = ["cat", "milk", "python", "hangman", "computer"]
+    word_to_guess = random.choice(wordlist)
+    length_of_word = len(word_to_guess)
+    print("Welcome to Hangman!")
+    print("_" * length_of_word)
 
-wordlist = ["cat", "milk", "python", "hangman", "computer"]
-word_to_guess = random.choice(wordlist)
-length_of_word = len(word_to_guess)
-print("Welcome to Hangman!")
-print("_" * length_of_word)
+    
