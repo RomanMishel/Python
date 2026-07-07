0e7b676d2ad7195fb55a75506cdafd76cb3dd6db
RomanMishel <sealaesone@gmail.com>
2026-07-07 16:22:40 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,10 +1,19 @@
-from random import choice, random
+from random import choice
 
 def hangman():
     wordlist = ["cat", "milk", "python", "hangman", "computer"]
-    word_to_guess = random.choice(wordlist)
+    word_to_guess = choice(wordlist)
     length_of_word = len(word_to_guess)
     print("Welcome to Hangman!")
     print("_" * length_of_word)
 
-    
+    user_guess = input("Guess a letter: ")
+
+    while True:
+        for index, letter in enumerate(word_to_guess):
+            display_word[index] = user_guess
+            print(" ".join(display_word))
+
+hangman()
+        
+
