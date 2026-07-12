c25c99b3fb71dadfa421cbdacfa367af2d1a5ba0
RomanMishel <sealaesone@gmail.com>
2026-07-12 20:59:28 +0300

    Update student_scaling.py

--- student_scaling.py
+++ student_scaling.py
@@ -3,8 +3,26 @@ def student_scaling():
         "John" : [90, 90, 100, 85, 85, 80],
         "Jane" : [80, 70, 80, 90, 70, 85],
         "Jim" : [100, 60, 80, 30, 50, 90],
-        "Jeff" : [90, 50, 70, 10, 80, 75]
+        "Jeff" : [90, 50, 70, 10, 80, 75],
+        "Jerry": [100, 100, 100, 95, 75]
     }
 
     for name, grades in students_list.items():
-        average_grade = 
+        average_grade = sum(grades) / len(grades)
+
+        print(name, "average:", average_grade)
+    
+        if average_grade >= 90:
+            print(students_list[name])
+            print("Great!")
+
+        elif average_grade >= 80:
+            print("Good!")
+
+        elif average_grade >= 70:
+            print("Mediocre...")
+
+        else:
+            print("Not good, this student needs to study harder.")
+
+student_scaling()
