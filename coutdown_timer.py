70f834a6db52d964ad3c22661056eb3929f6f0cf
RomanMishel <sealaesone@gmail.com>
2026-07-18 14:58:48 +0300

    Update coutdown_timer.py

--- coutdown_timer.py
+++ coutdown_timer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 def countdown():
     
@@ -9,4 +10,19 @@ def countdown():
     window.geometry("400x200")
 
     def timer():
-        
+        seconds = entry.get()
+        print(f"User has entered: {seconds}")
+
+        while seconds > 0:
+            minutes = seconds // 60
+            remaining_time = seconds % 60
+            time.sleep(1)
+            seconds -= 1
+        label = tk.Label(window, text=f"{minutes:02}:{remaining_time:02}", font=("Arial", 20))
+
+        label.pack(pady=20)
+
+        entry = tk.Entry(window)
+
+
+            
