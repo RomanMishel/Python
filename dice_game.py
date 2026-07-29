0e6972b3c114f742428a94a7848c6471775b6590
RomanMishel <sealaesone@gmail.com>
2026-07-29 20:58:35 +0300

    Update dice_game.py

--- dice_game.py
+++ dice_game.py
@@ -54,5 +54,62 @@ def dice_game():
         dice_num = random.randint(1,6)
         cpu_try = random.randint(1,6)
 
-        if dice_game > cpu_try:
-            
+        if dice_num > cpu_try:
+            if dice_num == 1:
+                print("Player wins!")
+                print(dice_1)
+
+            elif dice_num == 2:
+                print("Player wins!")
+                print(dice_2)
+
+            elif dice_num == 3:
+                print("Player wins!")
+                print(dice_3)
+
+            elif dice_num == 4:
+                print("Player wins!")
+                print(dice_4)
+
+            elif dice_num == 5:
+                print("Player wins!")
+                print(dice_5)
+
+            elif dice_num == 6:
+                print("Player wins!")
+                print(dice_6)
+
+            else:
+                print("Error")
+
+        else:
+            if cpu_try == 1:
+                print("CPU wins!")
+                print(dice_1)
+
+            elif cpu_try == 2:
+                print("CPU wins!")
+                print(dice_2)
+
+            elif cpu_try == 3:
+                print("CPU wins!")
+                print(dice_3)
+
+            elif cpu_try == 4:
+                print("CPU wins!")
+                print(dice_4)
+
+            elif cpu_try == 5:
+                print("CPU wins!")
+                print(dice_5)
+
+            elif cpu_try == 6:
+                print("CPU wins!")
+                print(dice_6)
+            else:
+                print("Error")
+    else:
+        print("Something went wrong!")
+        return
+
+dice_game()
