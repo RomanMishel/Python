95137dab752d856e6f445d9c3b9ee31f5e25c59a
RomanMishel <sealaesone@gmail.com>
2026-08-01 20:30:30 +0300

    .

---
+++ RPS_vs_CPU.py
@@ -0,0 +1,26 @@
+import random
+
+def RPS():
+    Rock = "1"
+    Paper = "2" 
+    Scissors = "3"
+        
+    player_1_choice = input("Player 1 choose your move: ")
+    cpu_choice = random.choice([Rock, Paper, Scissors])
+
+    if player_1_choice == cpu_choice :
+        print("Draw, Try again!")
+        return
+
+    elif player_1_choice == Scissors and cpu_choice == Paper:
+        print("Scissors beat Paper, Player 1 wins")
+        return
+    
+    elif player_1_choice == Rock and cpu_choice == Scissors:
+        print("Rock beats Scissors, Player 1 wins")
+        return
+    
+    else: 
+        print("CPU wins")
+        return
+RPS()
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -9,16 +9,15 @@ def RPS():
     if player_1_choice == player_2_choice :
         print("Draw ,Try again")
         return
-
-    elif player_1_choice == Rock and player_2_choice == Paper:
-        print("Rock beats Paper, Player 1 wins")
-        return
+    
     elif player_1_choice == Scissors and player_2_choice == Paper:
         print("Scissors beat Paper, Player 1 wins")
         return
+    
     elif player_1_choice == Rock and player_2_choice == Scissors:
         print("Rock beats Scissors, Player 1 wins")
         return
+    
     else: 
         print("Player 2 wins")
         return
