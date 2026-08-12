0dba7bea3d46ff2945a9561bf4546945de921e4c
RomanMishel <sealaesone@gmail.com>
2026-08-12 21:03:13 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -8,27 +8,25 @@ x_side = x
 y = int(input("Enter a Y length: "))
 y_side = y
 
-z = int(input("Ener a Z length: "))
+z = int(input("Enter a Z length: "))
 z_side = z
 
-if x_side or y_side or z_side == 0:
-    print("Sides cannot be equal to 0")
-    
+
+if x_side <= 0 or y_side <= 0 or z_side <= 0:
+    print("Sides must be greater than zero")
+
+elif (
+    x_side + y_side <= z_side
+    or x_side + z_side <= y_side
+    or y_side + z_side <= x_side
+):
+    print("Triangle does not exist")
+
+elif x_side == y_side == z_side:
+    print("Triangle is equilateral")
+
+elif x_side == y_side or x_side == z_side or y_side == z_side:
+    print("Triangle is isosceles")
+
 else:
-    if x_side == y_side == z_side:
-        print("Triangle is equal")
-
-    elif x_side == y_side < z_side:
-        print("Triangle sides unequal ")
-
-    elif x_side == y_side > z_side:
-        print("Triangle sides unproper")
-
-    elif x_side > y_side > z_side:
-        print("Sides are different")
-
-    elif x_side < y_side > z_side:
-        print("Sides are different")
-
-    else:
-        print("Error")
+    print("Triangle is scalene")
