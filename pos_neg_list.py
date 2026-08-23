e0bd1acf31ae24a1de994144a86ccb1f75de5596
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2026-08-23 21:15:46 +0300

    Create pos_neg_list.py

---
+++ pos_neg_list.py
@@ -0,0 +1,14 @@
+num_list = [5, -8, 2, -9 , 1, -1, -3, -5, 3]
+pos_list = []
+neg_list = []
+
+for num in num_list:
+    if num > 0:
+        pos_list.append(num)
+    elif num < 0:
+        neg_list.append(num)
+
+    else:
+        print("Error")
+print(pos_list)
+print(neg_list)
