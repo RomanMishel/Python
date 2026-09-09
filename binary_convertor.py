d0c3c21bef3c11bcd7ff81a8abb2998fe073a6e5
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2026-09-09 09:58:39 +0300

    Update binary_convertor.py

--- binary_convertor.py
+++ binary_convertor.py
@@ -1,2 +1,9 @@
-user_number = int(input("Введите число от 0 до 255: "))
+user_number = input("Введите число от 0 до 255: ")
+user_number = round(float(user_number))
 
+if  0 > user_number or user_number > 255:
+    print("Error! You cannot enter number higher than 255 or negative!")
+
+else:
+    binary_number = bin(user_number)[2:].zfill(8)
+    print(binary_number)
