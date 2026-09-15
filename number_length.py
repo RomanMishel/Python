ef309e2be6c63325a36ddd1c4db05fd50b7a17c5
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2026-09-15 21:50:28 +0300

    Update number_length.py

--- number_length.py
+++ number_length.py
@@ -1,6 +1,18 @@
 numbers = [1,1,1,2,3,2,3,5,4,2,3,1,5,3,2,4,6,6,5,1,6,7]
 
-max_items = []
+previous_num = numbers[0]
+number_count = 1
+max_count = 1
 
-min_items = []
+for x in numbers[1:]:
+    if x == previous_num:
+        number_count += 1
+    else:
+        number_count = 1
 
+    if number_count > max_count:
+        max_count = number_count
+
+    previous_num = x
+
+print("Самая длинная серия:", max_count)
