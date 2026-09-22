ed5dc090bb8ad53a88a6d9894da1b13a602c7fab
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2026-09-22 10:16:13 +0300

    Create pump.py

---
+++ codewars/pump.py
@@ -0,0 +1,6 @@
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    distance_to_pump = distance_to_pump - mpg * fuel_left
+    if distance_to_pump <= 0:
+        return True
+    else:
+        return False
